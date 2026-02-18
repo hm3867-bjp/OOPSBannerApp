3eb3917029cfcd90f88f25e7a1b0d0ed277fdb70
hm3867-bjp <hm3867@srmist.edu.in>
2026-02-18 09:03:59 +0530

    UC2: Added ASCII OOPS banner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,6 +1,14 @@
 public class OOPSBanner {
-     public static void main(String[] args)
-{
-System.out.println("welcome to OOPSBanner application");
-}
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****    ***** ");
+        System.out.println("*     * *     * *             *");
+        System.out.println("*     * *     * *       *     *");
+        System.out.println(" *****   *****  *        ***** ");
+
+    }
 }
